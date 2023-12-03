0f3f90dc81663820031987ba77dde60e8044c58e
Anastasiia-Shutka <151665460+Anastasiia-Shutka@users.noreply.github.com>
2023-12-03 17:30:13 +0200

    added class and method explanations in laboratorna.py
    Added explanations to classes and methods, fixed errors according to PEP8, added a command that is executed if the age of the horse does not belong to the given interval

--- laboratorna.py
+++ laboratorna.py
@@ -1,18 +1,17 @@
-"""
-module
-"""
-
-
 class RaceHorse:
     """
-    class
+    Class representing a horse.
     """
     def __init__(self, name, speed, age):
+        """
+        Initializes a horse object with the given name and age.
+        """
         self.name = name
         self.speed = speed
         self.age = age
         self.place_in_race = None
 
+    
     def __str__(self):
         return f"{self.name}: Speed - {self.speed},\
          Age - {self.age}, Place in Race - {self.place_in_race}"
@@ -20,28 +19,33 @@ class RaceHorse:
 
 class Race:
     """
-    class
+    Class representing a horse race.
     """
     def __init__(self):
+        """
+        Initializes a race object with an empty list of participants.
+        """
         self.participants = []
 
     def add_horse(self, horse):
         """
-        add horse
+        Adds a horse to the race if it is between 3 and 7 years old (inclusive).
         """
         if 3 <= horse.age <= 7:
-            self.participants.append(horse)
+        else:
+            print(f"Sorry, {horse.name} is not eligible for the race.")
+        self.participants.append(horse)
 
     def remove_horse(self, horse):
         """
-        remove
+        Removes a horse from the race.
         """
         if horse in self.participants:
             self.participants.remove(horse)
 
     def race(self):
         """
-        race
+        Conducts a race and prints the results.
         """
         if not self.participants:
             print("No horses in the race.")
